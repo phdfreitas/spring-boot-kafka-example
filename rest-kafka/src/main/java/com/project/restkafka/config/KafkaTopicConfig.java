967892e65fb6b87516a8cddb9283359ebc93c757
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-10-13 12:23:10 -0300

    producer, consumer and topic configuration

---
+++ rest-kafka/src/main/java/com/project/restkafka/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package com.project.restkafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic kafkaExampleProject(){
+        return TopicBuilder.name("kafkaExample")
+                .build();
+    }
+
+}
