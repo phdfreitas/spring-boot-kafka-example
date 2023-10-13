e3245d45c8640088bfe37234131e5c5b601f74aa
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-10-13 14:13:52 -0300

    kafka producer

---
+++ rest-kafka/src/main/java/com/project/restkafka/model/Person.java
@@ -0,0 +1,41 @@
+package com.project.restkafka.model;
+
+public class Person {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
